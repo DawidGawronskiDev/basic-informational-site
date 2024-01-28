51db6f1d8dd9c0d073eaff8b99d4e5eea01a693c
DawidGawronskiDev <dawidgawronskicontact@gmail.com>
2024-01-28 14:12:42 +0000

    Add title variable

--- index.js
+++ index.js
@@ -5,15 +5,15 @@ const app = express()
 app.set("view engine", "ejs");
 
 app.get("/", (req, res) => {
-    res.render("index")
+    res.render("index", { title: "Home" })
 })
 
 app.get("/about", (req, res) => {
-    res.render("about")
+    res.render("about", { title: "About" })
 })
 
 app.get("/contact-me", (req, res) => {
-    res.render("contact-me")
+    res.render("contact-me", { title: "Contact Me" })
 })
 
 app.get("/contact", (req, res) => {
@@ -21,7 +21,7 @@ app.get("/contact", (req, res) => {
 })
 
 app.use((req, res) => {
-    res.render("404")
+    res.render("404", { title: "404" })
 })
 
 app.listen(8080)
